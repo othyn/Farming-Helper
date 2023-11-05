4078a151a931b728175e18677e1e55e210188f55
Ben <git@othyn.com>
2023-11-05 17:10:13 +0000

    Bump the position of the house tele option below the new pay protection option

--- src/main/java/com/farminghelper/speaax/FarmingHelperConfig.java
+++ src/main/java/com/farminghelper/speaax/FarmingHelperConfig.java
@@ -28,7 +28,7 @@ public interface FarmingHelperConfig extends Config
 		String name();
 	}
 	@ConfigItem(
-			position = 9,
+			position = 10,
 			keyName = "enumConfigHouseTele",
 			name = "House teleport",
 			description = "Desired way to teleport to house",
