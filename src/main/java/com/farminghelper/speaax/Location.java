e12540687ee2a02f00ad34b06b9e754bbd0cd141
Ben <git@othyn.com>
2023-11-11 17:46:01 +0000

    Reformat

--- src/main/java/com/farminghelper/speaax/Location.java
+++ src/main/java/com/farminghelper/speaax/Location.java
@@ -1,16 +1,17 @@
 package com.farminghelper.speaax;
 
-import java.awt.*;
+import net.runelite.api.coords.WorldPoint;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
-import net.runelite.api.coords.WorldPoint;
-
-public class Location {
-    public enum TeleportCategory {
+public class Location
+{
+    public enum TeleportCategory
+    {
         ITEM,
         PORTAL_NEXUS,
         SPIRIT_TREE,
@@ -18,22 +19,29 @@ public class Location {
         MOUNTED_XERICS,
         SPELLBOOK
     }
+
     private String name;
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
+
     private Boolean farmLimps;
 
-    public Boolean getFarmLimps() {
+    public Boolean getFarmLimps()
+    {
         return farmLimps;
     }
 
     private List<Teleport> teleportOptions;
+
     private FarmingHelperConfig config;
+
     private final Function<FarmingHelperConfig, FarmingHelperConfig.OptionEnumTeleport> selectedTeleportFunction;
 
-    public Location(Function<FarmingHelperConfig, FarmingHelperConfig.OptionEnumTeleport> selectedTeleportFunction, FarmingHelperConfig config, String name, Boolean farmLimps) {
+    public Location(Function<FarmingHelperConfig, FarmingHelperConfig.OptionEnumTeleport> selectedTeleportFunction, FarmingHelperConfig config, String name, Boolean farmLimps)
+    {
         this.config = config;
         this.selectedTeleportFunction = selectedTeleportFunction;
         this.name = name;
@@ -41,11 +49,13 @@ public class Location {
         this.teleportOptions = new ArrayList<>();
     }
 
-    public void addTeleportOption(Teleport teleport) {
+    public void addTeleportOption(Teleport teleport)
+    {
         teleportOptions.add(teleport);
     }
 
-    public Teleport getSelectedTeleport() {
+    public Teleport getSelectedTeleport()
+    {
         String selectedEnumOption = selectedTeleportFunction.apply(config).name();
         for (Teleport teleport : teleportOptions) {
             if (teleport.getEnumOption().equalsIgnoreCase(selectedEnumOption)) {
@@ -55,26 +65,32 @@ public class Location {
         return teleportOptions.isEmpty() ? null : teleportOptions.get(0);
     }
 
-
-
-
-
-    public class Teleport {
+    public class Teleport
+    {
         private TeleportCategory category;
+
         private String description;
-        private Color color;
+
         private int id;
+
         private int interfaceGroupId;
+
         private int interfaceChildId;
 
         private int regionId;
+
         private String enumOption;
+
         private String rightClickOption;
+
         private List<ItemRequirement> itemRequirements;
+
         private WorldPoint point;
+
         private String overrideLocationName;
 
-        public Teleport(String enumOption, TeleportCategory category, String description, int id, String rightClickOption, int interfaceGroupId, int interfaceChildId, int regionId, WorldPoint point, List<ItemRequirement> itemRequirements) {
+        public Teleport(String enumOption, TeleportCategory category, String description, int id, String rightClickOption, int interfaceGroupId, int interfaceChildId, int regionId, WorldPoint point, List<ItemRequirement> itemRequirements)
+        {
             this.enumOption = enumOption;
             this.category = category;
             this.description = description;
@@ -88,26 +104,20 @@ public class Location {
             this.overrideLocationName = "";
         }
 
-        public Map<Integer, Integer> getItemRequirements() {
+        public Map<Integer, Integer> getItemRequirements()
+        {
             Map<Integer, Integer> requirements = new HashMap<>();
+
             for (ItemRequirement itemRequirement : itemRequirements) {
                 requirements.put(itemRequirement.getItemId(), itemRequirement.getQuantity());
             }
+
             return requirements;
         }
 
-        public WorldPoint getPoint(){return point;}
-
-        public void addItemRequirement(int itemId, int quantity) {
-            ItemRequirement itemRequirement = new ItemRequirement(itemId, quantity);
-            this.itemRequirements.add(itemRequirement);
-        }
-        public void addAllItemRequirements(List<ItemRequirement> itemRequirements) {
-            this.itemRequirements.addAll(itemRequirements);
-        }
-
-        public void updateTeleportItemId(int newItemId) {
-            this.id = newItemId;
+        public WorldPoint getPoint()
+        {
+            return point;
         }
 
         public Location.Teleport overrideLocationName(String overrideLocationName)
@@ -122,53 +132,44 @@ public class Location {
             return this.overrideLocationName;
         }
 
-        public TeleportCategory getCategory() {
+        public TeleportCategory getCategory()
+        {
             return category;
         }
-        public int getRegionId() {
+
+        public int getRegionId()
+        {
             return regionId;
         }
-        public String getEnumOption() {
+
+        public String getEnumOption()
+        {
             return enumOption;
         }
 
-        public String getDescription() {
+        public String getDescription()
+        {
             return description;
         }
-        public String getRightClickOption() {
+
+        public String getRightClickOption()
+        {
             return rightClickOption;
         }
 
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public int getId() {
+        public int getId()
+        {
             return id;
         }
 
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public int getInterfaceGroupId() {
+        public int getInterfaceGroupId()
+        {
             return interfaceGroupId;
         }
 
-        public void setInterfaceGroupId(int interfaceGroupId) {
-            this.interfaceGroupId = interfaceGroupId;
-        }
-
-        public int getInterfaceChildId() {
+        public int getInterfaceChildId()
+        {
             return interfaceChildId;
-        }
-
-        public void setInterfaceChildId(int interfaceChildId) {
-            this.interfaceChildId = interfaceChildId;
-        }
-
-        public Color getColor() {
-            return  color;
         }
     }
 }
--- src/main/java/com/farminghelper/speaax/UI/StartStopJButton.java
+++ src/main/java/com/farminghelper/speaax/UI/StartStopJButton.java
@@ -12,7 +12,7 @@ public class StartStopJButton extends JButton
         super(text, null);
 
         this.originalText = text;
-        
+
         this.setStartStopState(false);
     }
 
